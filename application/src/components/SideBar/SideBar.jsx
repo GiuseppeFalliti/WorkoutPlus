1a253e346035ad6502b72e0d894cfc4850d2cdd2
donaldohoxha <donaldo.hoxha06@gmail.com>
2025-05-28 13:12:50 +0200

    position

--- application/src/components/SideBar/SideBar.jsx
+++ application/src/components/SideBar/SideBar.jsx
@@ -38,12 +38,12 @@ const SideBar = ({ isOpen, setIsOpen }) => {
       {/* Bottone hamburger per mobile */}
       {isMobile ? (isOpen ?<button 
           onClick={() => setIsOpen(!isOpen)}
-          className="absolute top-3 left-15 text-white"
+          className="relative top-3 left-15 text-white"
         >
           <FaBars className="text-xl" />
         </button> : <button 
           onClick={() => setIsOpen(!isOpen)}
-          className="relative top-3 left-5 text-white"
+          className="relative top-2 left-2 text-white"
         >
           <FaBars className="text-xl" />
         </button> ) : null} 
